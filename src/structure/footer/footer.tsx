b1aa29214090c8d2265690fa5d98b38920932e2b
Felipe Oliveira <felipephito@gmail.com>
2025-06-11 00:14:51 -0300

    feat: Implements initial setup (#1)

---
+++ src/structure/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-6 bg-black flex items-center justify-center text-white'>Footer</footer>
+  )
+}
+
+export default Footer
---
+++ src/structure/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='sticky h-24 bg-green-dark flex items-center justify-center text-white'>Header</header>
+  )
+}
+
+export default Header
