0293a960ea6e83133541ceeb570464d5768bbf57
Manfrin92 <manfredd_10@hotmail.com>
2025-06-20 13:38:57 -0300

    fix: add common layout for all pages

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,27 @@
+import Footer from "@/structure/footer";
+import Header from "@/structure/header";
 import "@/styles/globals.css";
+import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <NextSeo title="Bytebank | Homepage" />
+      <div className={`${inter.className} font-[family-name:var(--font-inter)]`}>
+        <Header />
+        <main className="min-h-screen">
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
 }
