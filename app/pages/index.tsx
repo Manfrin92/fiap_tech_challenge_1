91d383f696282f4166af685753cc8a4a75d94456
Simone Lopes <sisilopes20@gmail.com>
2025-06-20 22:22:16 +0100

    feat: create hook to use localstorage (#3)

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,13 +2,16 @@ import { NextSeo } from 'next-seo'
 import Footer from "@/structure/footer";
 import Header from "@/structure/header";
 import { Inter } from "next/font/google";
+import useLocalStorage from '@/hooks/useLocalStorage';
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-export default function Home() {
+export default function Home() {  
+   const [valueStorage, setValueStorage, getValueStorage] = useLocalStorage("welcome", "This item was get in Local Storage")
+
   return (
     <>
       <NextSeo
@@ -18,7 +21,11 @@ export default function Home() {
         className={`${inter.className} font-[family-name:var(--font-inter)]`}
       >
         <Header />
-        <main className='min-h-screen'></main>
+        <main className='min-h-screen'>
+          {/* <h1>{valueStorage}</h1>
+          <button className='bg-green-dark text-white mr-3 p-2 mt-3' onClick={() => setValueStorage("You are here again")}>Click me</button>
+          <button className='bg-green-light' onClick={() => alert(getValueStorage())}>Ler direto do localStorage</button> */}
+        </main>
         <Footer />
       </div>
     </>
