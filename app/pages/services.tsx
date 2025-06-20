60f6849df9bcadf9ef1f79d5c363b3b416173068
Manfrin92 <manfredd_10@hotmail.com>
2025-06-20 19:01:25 -0300

    feat: add service page

---
+++ app/pages/services.tsx
@@ -0,0 +1,11 @@
+import ServicesOptions from "@/components/services/ServiceOptions";
+import WelcomeCard from "@/components/services/WelcomeCard";
+
+export default function ServicesPage() {
+  return (
+    <div className="flex flex-col mt-6 gap-6">
+      <WelcomeCard />
+      <ServicesOptions />
+    </div>
+  );
+}
