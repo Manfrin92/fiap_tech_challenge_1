50a0624c367e2b6b7b28b9a8c676296dd45a1189
Manfrin92 <manfredd_10@hotmail.com>
2025-06-23 18:23:51 -0300

    fix: avoiding hardcoded values from beggining

--- app/components/services/WelcomeCard.tsx
+++ app/components/services/WelcomeCard.tsx
@@ -1,18 +1,46 @@
-import Eye from "@/assets/icons/eye.svg";
+import Eye from '@/assets/icons/eye.svg';
 
-export default function WelcomeCard() {
+interface WelcomeCardProps {
+  name: string;
+  balance: number;
+}
+
+export default function WelcomeCard({ name, balance }: WelcomeCardProps) {
+  const today = new Date();
+  const balanceFormatted = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(balance);
+
+  const todayFormatted = new Intl.DateTimeFormat('pt-BR', {
+    weekday: 'long',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  }).format(today);
+
   return (
     <div className="flex flex-col bg-primary p-8 pb-7 pr-30 rounded-lg max-w-210 min-h-100">
-      <h5 className="font-bold text-white text-2xl mb-6">Olá, Joana! :)</h5>
-      <span className="text-white text-sm">Quarta-feira, 08/09/2024</span>
+      <h5 className="font-bold text-white text-2xl mb-6">Olá, {name}! :)</h5>
+      <span className="text-white text-sm">{todayFormatted}</span>
       <div className="flex flex-col self-end">
         <div className="flex items-center gap-6">
           <h4 className="font-semibold text-white text-xl">Saldo</h4>
-          <Eye className="text-2xl"/>
+          <Eye className="text-2xl" />
         </div>
-        <hr className="mt-4" style={{ border: "none", height: "2px", backgroundColor: "red", width: "100%" }} />
+        <hr
+          className="mt-4"
+          style={{
+            border: 'none',
+            height: '2px',
+            backgroundColor: 'red',
+            width: '100%',
+          }}
+        />
         <span className="text-white text-base mt-4">Conta corrente</span>
-        <span className="font-bold text-white text-3xl mt-2">R$ 2.500,00</span>
+        <span className="font-bold text-white text-3xl mt-2">
+          {balanceFormatted}
+        </span>
       </div>
     </div>
   );
--- app/pages/services.tsx
+++ app/pages/services.tsx
@@ -1,12 +1,18 @@
-import ServicesOptions from "@/components/services/ServiceOptions";
-import WelcomeCard from "@/components/services/WelcomeCard";
-import { NextSeo } from "next-seo";
+import ServicesOptions from '@/components/services/ServiceOptions';
+import WelcomeCard from '@/components/services/WelcomeCard';
+import { NextSeo } from 'next-seo';
 
 export default function ServicesPage() {
+  // TODO: get it from external API
+  const accountOwner = {
+    name: 'Joana',
+    balance: 2500,
+  };
+
   return (
     <div className="container">
       <NextSeo title="Bytebank | Services" />
-      <WelcomeCard />
+      <WelcomeCard name={accountOwner.name} balance={accountOwner.balance} />
       <ServicesOptions />
     </div>
   );
