84fc0f42c06bb76b011d48264341eab523292238
Simone <sisilopes20@gmail.com>
2025-06-23 22:39:35 +0100

    config: change clsx lib to tailwind-variants

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import clsx from "clsx";
+import { tv } from "tailwind-variants";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   error?: string;
 }
 
+const inputVariants = tv({
+  base: "border rounded-md px-3 py-2 text-1 text-black focus:outline-none",
+  variants: {
+    error: {
+      true: "border-[var(--color-input-error)]",
+      false: "border-[var(--color-input-text)]",
+    },
+  },
+});
+
 const Input = ({ className, error, ...props }: InputProps) => {
   return (
     <div className="flex flex-col gap-2">
       <input
-        className={clsx(
-          "border rounded-md px-3 py-2 text-1 text-black focus:outline-none",
-          error ? "border-[var(--color-input-error)]" : "border-[var(--color-input-text)]",
-          className
-        )}
+        className={`${inputVariants({ error: !!error })} ${className ?? ""}`}
         {...props}
       />
 
