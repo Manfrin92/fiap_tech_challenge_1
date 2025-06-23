fa8fb20dada864d584d96626d9055671d4784d3c
Manfrin92 <manfredd_10@hotmail.com>
2025-06-23 17:54:47 -0300

    feat: NextSEO

--- app/pages/services.tsx
+++ app/pages/services.tsx
@@ -1,9 +1,11 @@
 import ServicesOptions from "@/components/services/ServiceOptions";
 import WelcomeCard from "@/components/services/WelcomeCard";
+import { NextSeo } from "next-seo";
 
 export default function ServicesPage() {
   return (
     <div className="container">
+      <NextSeo title="Bytebank | Services" />
       <WelcomeCard />
       <ServicesOptions />
     </div>
