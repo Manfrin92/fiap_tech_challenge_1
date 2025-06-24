a626152d798f2e13bdd18b1decbb50b85f72d0e2
Simone Lopes <sisilopes20@gmail.com>
2025-06-24 17:11:44 +0100

    Feature/input (#9)
    * wip: start building Input component
    
    * feat: create input component
    
    * config: change clsx lib to tailwind-variants
    
    * Update package.json

---
+++ app/components/input/Input.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { tv } from "tailwind-variants";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  error?: string;
+}
+
+const inputVariants = tv({
+  base: "border rounded-md px-3 py-2 text-1 text-black focus:outline-none",
+  variants: {
+    error: {
+      true: "border-[var(--color-input-error)]",
+      false: "border-[var(--color-input-text)]",
+    },
+  },
+});
+
+const Input = ({ className, error, ...props }: InputProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className={`${inputVariants({ error: !!error })} ${className ?? ""}`}
+        {...props}
+      />
+
+      {error && (
+        <span className="text-sm text-[var(--color-input-error)]">{error}</span>
+      )}
+    </div>
+  );
+};
+
+export default Input;
