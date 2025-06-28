da9b1079b0a88672645a9759219fe8a775810652
Manfrin92 <manfredd_10@hotmail.com>
2025-06-28 12:55:56 -0300

    fix: extracting icons into array

--- app/components/services/services.tsx
+++ app/components/services/services.tsx
@@ -7,6 +7,35 @@ import CellPhone from '@/assets/icons/cell-phone.svg'
 import ServiceCard from './service-card'
 
 const Services = () => {
+  const serviceSvgClassName = "md:text-6xl text-7xl"
+
+  const serviceCards = [
+    {
+      name: "Empréstimo",
+      icon: <Loan className={serviceSvgClassName} />
+    },
+    {
+      name: "Meus cartões",
+      icon: <Card className={serviceSvgClassName} />
+    },
+    {
+      name: "Doações",
+      icon: <Donation className={serviceSvgClassName} />
+    },
+    {
+      name: "Pix",
+      icon: <Pix className={serviceSvgClassName} />
+    },
+    {
+      name: "Seguros",
+      icon: <Insurance className={serviceSvgClassName} />
+    },
+    {
+      name: "Crédito celular",
+      icon: <CellPhone className={serviceSvgClassName} />
+    },
+  ]
+
   return (
   <section className="flex flex-col bg-background-service-options p-8 pb-7 rounded-lg gap-8">
     <h5 className="font-bold text-black text-2xl">
@@ -14,12 +43,9 @@ const Services = () => {
       <span className="hidden sm:block xs:block">Nova transação</span>
     </h5>
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-5 sm:gap-6 md:gap-7">
-      <ServiceCard icon={<Loan className="md:text-6xl text-7xl" />} name="Empréstimo" />
-      <ServiceCard icon={<Card className="md:text-6xl text-7xl" />} name="Meus cartões" />
-      <ServiceCard icon={<Donation className="md:text-6xl text-7xl" />} name="Doações" />
-      <ServiceCard icon={<Pix className="md:text-6xl text-7xl" />} name="Pix" />
-      <ServiceCard icon={<Insurance className="md:text-6xl text-7xl" />} name="Seguros" />
-      <ServiceCard icon={<CellPhone className="md:text-6xl text-7xl" />} name="Crédito celular" />
+      {serviceCards.map((serviceCard, i) => (
+        <ServiceCard key={`${serviceCard.name}-${i}`} icon={serviceCard.icon} name={serviceCard.name} />
+      ))}
     </div>
   </section>
   )
